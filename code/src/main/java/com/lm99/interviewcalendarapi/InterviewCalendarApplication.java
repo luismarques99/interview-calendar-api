90374c6e2efc6eb47d8d5ef520141fb813e0e616
Luis Marques <luisserafim99@hotmail.com>
2023-06-21 12:12:26 +0100

    Add initial Spring configurations

---
+++ code/src/main/java/com/lm99/interviewcalendarapi/InterviewCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.lm99.interviewcalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewCalendarApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/com/lm99/interviewcalendarapi/InterviewCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm99.interviewcalendarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
